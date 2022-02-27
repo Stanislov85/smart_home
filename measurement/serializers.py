5d3d2959ae019af591d8001378659b983c7be622
StanislavPonomarev <stanislov@inbox.ru>
2022-02-27 21:43:22 +0300

    Initial commit

---
+++ measurement/serializers.py
@@ -0,0 +1,15 @@
+from rest_framework import serializers
+
+# TODO: опишите необходимые сериализаторы
+from .models import Sensor,Measurement
+
+class SensorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sensor
+        fields = '__all__'
+
+class MeasurementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Measurement
+        fields = '__all__'
+
---
+++ measurement/views.py
@@ -0,0 +1,21 @@
+# TODO: опишите необходимые обработчики, рекомендуется
+#  использовать generics APIView классы:
+# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework.viewsets import ModelViewSet
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, MeasurementSerializer
+
+class SensorViewSet(ModelViewSet):
+    """ViewSet для проекта."""
+    # TODO: добавьте конфигурацию для объекта
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class MeasurementViewSet(ModelViewSet):
+    """ViewSet для измерения."""
+    # TODO: добавьте конфигурацию для измерения
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
